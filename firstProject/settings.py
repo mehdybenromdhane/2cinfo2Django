2ae4dcf478c6f1eddfe03ee39b4a0f5b6de23d37
unknown <mehdy.benromdhane@esprit.tn>
2025-07-05 14:34:35 +0100

    fonction views hello

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'firstProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
