479705935889f2880a935d7ee788064b93f269e6
unknown <mehdy.benromdhane@esprit.tn>
2025-07-05 14:10:51 +0100

    admin

--- Person/admin.py
+++ Person/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Person
-admin.site.register(Person)
+
+
+# admin.site.register(Person)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    
+    search_fields=('username',)
